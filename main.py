0081b0e484b839211c303a126c4d23098202370c
Ryan Faiz Sanie <hi@ryanfaiz.com>
2024-11-07 20:49:23 +0700

    feat: Add banner in startup

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ headers = [
 minimal_headers = ["Waktu Gempa (UTC)", "Magnitudo", "Wilayah", "Status"]
 minimal_headers_index = [0,3,5,6]
 column_widths = [18, 10, 30, 14]
+
+def show_banner():
+    print("=" * 40)
+    print("|            EarthQuake                |")
+    print("|  Sistem Monitoring Gempa Indonesia   |")
+    print("=" * 40)
+
+
+show_banner()
