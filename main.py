ba8c20cd1442773ca2d09da150f115a6b82a3b84
Ryan Faiz Sanie <hi@ryanfaiz.com>
2024-11-07 20:55:00 +0700

    feat: add create new record functionality

--- main.py
+++ main.py
@@ -92,6 +92,65 @@ def show_latest_earthquake():
     print(f"  Status: {latest_record[6]}")
     print(f"  Detail: {latest_record[7]}")
 
+def create_record(record):
+    current_records = read_all_record()
+    
+    current_records.insert(1, record)
+
+    with open(csv_file, mode="w", newline="", encoding="utf-8-sig") as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerows(current_records)
+    
+    return "Data berhasil ditambahkan."
+
+def validate_form(record: list):
+    desc = []
+
+    for item in record:
+        if item == "":
+            desc.append("Data tidak boleh kosong.")
+            break
+
+    if record[6] != "Confirmed" and record[6] != "Not Confirmed":
+        desc.append("Status tidak sesuai.")
+
+    return desc
+
+def add_record_form():
+    waktu = input("Waktu Gempa (UTC) (DD/MM/YYYY HH:MM): ")
+    lintang = input("Lintang: ")
+    bujur = input("Bujur: ")
+    magnitudo = input("Magnittudo: ")
+    kedalaman = input("Kedalaman (Km): ")
+    wilayah = input("Wilayah: ")
+    status = input("Status (Confirmed/Not Confirmed): ")
+    detail = input("Detail (Link BMKG): ")
+
+    record = [waktu, lintang, bujur, magnitudo, kedalaman, wilayah, status, detail]
+
+    result = validate_form(record)
+
+    if result:
+        for desc in result:
+            print(desc)
+        print("1. Coba lagi")
+        print("2. Kembali ke menu utama")
+        nav = int(input("Pilih opsi (1-2): "))
+        if nav == 1:
+            add_record_form()
+        else:
+            return
+    lintang = round(int(lintang), 2)
+    bujur = round(int(bujur), 2)
+    magnitudo = int(magnitudo)
+    kedalaman = int(kedalaman)
+
+    record = [waktu, lintang, bujur, magnitudo, kedalaman, wilayah, status, detail]
+
+    result = create_record(record)
+    
+    return print(result)
+
 
 show_banner()
 show_latest_earthquake()
