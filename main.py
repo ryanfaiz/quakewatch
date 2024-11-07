9f84aa6431dc48a1e7e28f5809b8ac6c6b3a000d
Ryan Faiz Sanie <hi@ryanfaiz.com>
2024-11-07 20:48:44 +0700

    feat: Add file metadata

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import csv
 import os
 
+csv_file = "data-gempa.csv"
+headers = [
+    "Waktu Gempa (UTC)",
+    "Lintang",
+    "Bujur",
+    "Magnitudo",
+    "Kedalaman (Km)",
+    "Wilayah",
+    "Status",
+    "Detail"
+]
+minimal_headers = ["Waktu Gempa (UTC)", "Magnitudo", "Wilayah", "Status"]
+minimal_headers_index = [0,3,5,6]
+column_widths = [18, 10, 30, 14]
